989ce04e863aa01ba481eec8a2bc987ef294c7a8
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-31 01:54:44 -0400

    update

---
+++ social/pages/home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div>
+        <header>
+            <div>
+                <Image />
+            </div>
+        </header>
+
+    </div>
+  )
+}
+
+export default Home
